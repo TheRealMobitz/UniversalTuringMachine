1e06435a8f8eb788245df842b5ddce2ebb6248a2
Mobeen <therealmobitz@outlook.com>
2024-07-02 11:27:34 +0330

    parsing implementation
    implemented parser, turns txt files into unary data

---
+++ turing_machine.py
@@ -0,0 +1,58 @@
+import re
+
+def parse_turing_machine(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+
+    states = re.findall(r'states:\{(.*?)\}', content)[0].split(',')
+    start_state = re.findall(r'start_state:\{(.*?)\}', content)[0]
+    final_states = re.findall(r'final_states:\{(.*?)\}', content)[0].split(',')
+    actions = re.findall(r'actions:\{(.*?)\}', content)[0].split('),(')
+
+    actions = [action.replace('(', '').replace(')', '').split(',') for action in actions]
+
+    alphabet = set()
+    for action in actions:
+        alphabet.add(action[1])
+        alphabet.add(action[2])
+    print('alph=',alphabet)
+
+    turing_machine = {
+        'states': states,
+        'start_state': start_state,
+        'final_states': final_states,
+        'actions': actions,
+        'alphabet': alphabet
+    }
+
+    return turing_machine
+
+print (parse_turing_machine("./test.txt"))
+
+def convert_to_unary(turing_machine):
+    state_mapping = {state: '1' * (i + 1) for i, state in enumerate(turing_machine['states'])}
+    alphabet_mapping = {alphabet: '1' * (i+1) for i, alphabet in enumerate(turing_machine['alphabet'])}
+    
+    start_state_unary = state_mapping[turing_machine['start_state']]
+    final_states_unary = [state_mapping[state] for state in turing_machine['final_states']]
+    
+    actions_unary = []
+    for action in turing_machine['actions']:
+        current_state = state_mapping[action[0]]
+        read_symbol = alphabet_mapping[action[1]]
+        write_symbol = alphabet_mapping[action[2]]
+        direction = '1' if action[3] == 'R' else '11'
+        next_state = state_mapping[action[4]]
+        
+        actions_unary.append((current_state, read_symbol, write_symbol, direction, next_state))
+    
+    unary_turing_machine = {
+        'states': list(state_mapping.values()),
+        'start_state': start_state_unary,
+        'final_states': final_states_unary,
+        'actions': actions_unary
+    }
+    
+    return unary_turing_machine
+
+print(convert_to_unary(parse_turing_machine("./test.txt")))
