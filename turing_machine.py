9ce7dfb1e0a7772b0c8fde2dca29a103c507de7c
reza-ma82 <rm13822831@gmail.com>
2024-07-03 17:35:49 +0330

    Add Universal Turing Machine (UTM) Simulation

--- turing_machine.py
+++ turing_machine.py
@@ -81,7 +81,7 @@ def state_tape_init(states,start_state, final_states):
     for state in final_states:
         state_tape += state
     
-    return state_tape[:-1]
+    return state_tape
 
 
 print("state tape:", state_tape_init(TM['states'], TM['start_state'], TM['final_states']))
@@ -94,3 +94,138 @@ class UTM():
         self.description_tape = description_tape
         self.content_tape = content_tape
         self.state_tape = state_tape
+        self.state_index = 0
+
+
+    def run_turing_machine(self, index_content_tape):
+        current_state = self.calculate_current_state()
+        current_content = self.calculate_current_content(index_content_tape)
+        i=0
+        print(f"content tape before change: \n{self.content_tape}")
+        print(index_content_tape*" " +"^")
+        while i < len(description_tape):
+            print(f"description tape: \n{self.description_tape}")
+            print(i*" " +"^")
+            state = ""
+            next_state = ""
+            input = ""
+            output = ""
+            direction = ""
+            while self.description_tape[i] == "1":
+                state += "1"
+                i+=1
+            i+=1
+            while self.description_tape[i] == "1":
+                input += "1"
+                i+=1
+            i+=1
+            while self.description_tape[i] == "1":
+                output += "1"
+                i+=1
+            i+=1
+            while self.description_tape[i] == "1":
+                direction += "1"
+                i+=1
+            i+=1
+            while i < len(self.description_tape) and self.description_tape[i] == "1":
+                next_state += "1"
+                i+=1
+            if i < len(self.description_tape): 
+                i+=2
+            # print(f"state: {state} next: {next_state}")
+            # print(f"input: {input} output: {output}")
+            # print(f"current state: {current_state} current input: {current_content}")
+            
+            if state == current_state and input == current_content:
+                self.change_current_state(next_state)
+                self.change_current_content(output, index_content_tape)
+                index_content_tape = self.apply_direction(direction, index_content_tape)
+                # print(f"state index: {self.state_index}")
+                # print(f"content index: {index_content_tape}")
+                # print(f"description index: {i}")
+                print(f"content tape after change: \n{self.content_tape}")
+                print(index_content_tape*" " +"^")
+                return self.run_turing_machine(index_content_tape)
+        # print(self.content_tape)
+        return self.halt(current_state)
+
+    def halt(self,current_state):
+        i = len(self.state_tape)-1
+        while True:
+            final_state = ""
+            while self.state_tape[i] != "0":
+                print(f"state tape: \n{self.state_tape}")
+                print(i*" " +"^")
+                final_state += "1"
+                i -= 1
+            i-=1
+            print("final state: "+final_state)
+            if current_state == final_state:
+                return 1
+            if self.state_tape[i] == "0":
+                return 0
+        
+    def calculate_current_state(self):
+        i = 0
+        state = ""
+        while self.state_tape[i + self.state_index] != "0":
+            state += "1"
+            i += 1
+        print("calculate current state: "+state)
+        return state
+    
+    def calculate_current_content(self, index_content_tape):
+        content = ""
+        i = 0
+        while self.content_tape[i + index_content_tape] != "0":
+            content += "1"
+            i +=1
+        print("calculate current content: "+content)
+        return content
+
+    def change_current_state(self, next_state):
+        i = 0
+        while True:
+            state = ""
+            while self.state_tape[i] != "0":
+                state += "1"
+                i += 1
+            i += 1
+            # print(f"state is {state} and next is : {next_state}")
+            if(next_state == state):
+                break
+        i -= 2
+        while self.state_tape[i] != "0" and i>0:
+                i -= 1
+        if i >0:
+            i += 1
+        self.state_index = i
+    
+    def change_current_content(self, output, index_content_tape):
+        index_end_content_tape = index_content_tape
+        while self.content_tape[index_end_content_tape] != "0":
+            index_end_content_tape += 1
+        self.content_tape = self.content_tape[:index_content_tape] + output + self.content_tape[index_end_content_tape:]
+
+    def apply_direction(self,direction,index_content_tape):
+        if direction=='1' : #R
+            while self.content_tape[index_content_tape] != "0" and index_content_tape < len(self.content_tape):
+                index_content_tape += 1
+            # TODO: need some condition here for overloading
+            index_content_tape += 1
+        else: #L
+            index_content_tape -= 2
+            while self.content_tape[index_content_tape] != "0" and index_content_tape > 0:
+                index_content_tape -= 1
+            if index_content_tape != 0:
+                index_content_tape += 1
+            # TODO: need some condition here for overloading
+        return index_content_tape
+
+description_tape = description_tape_init(TM['actions'])
+state_tape = state_tape_init(TM['states'], TM['start_state'], TM['final_states'])
+
+uni = UTM(TM['start_state'],TM['final_states'],description_tape,"110101011011011011",state_tape)
+print(uni.run_turing_machine(3))
+
+# TODO: translate contenet tape
